d42ddc1b0b3046444dfb841d984b79dfc57c4ea3
undefined2023 <undefined2023@outlook.com>
2020-11-09 01:02:17 +0800

    trying to add customized md5 encoder

---
+++ src/main/java/com/webencyclop/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.webencyclop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+//		String md5 = WebMvcConfig.getMd5("hgb930207");
+//		System.out.println(md5);
+//		if (true) return;
+
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+}
---
+++ src/main/java/com/webencyclop/demo/WebMvcConfig.java
@@ -0,0 +1,96 @@
+package com.webencyclop.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new PasswordEncoder() {
+			@Override
+			public String encode(CharSequence charSequence) {
+				return getMd5(charSequence.toString());
+			}
+
+			@Override
+			public boolean matches(CharSequence charSequence, String s) {
+				return getMd5(charSequence.toString()).equals(s);
+			}
+		};
+	}
+
+	public static String getMd5(String input) {
+		try {
+			// Static getInstance method is called with hashing SHA
+			MessageDigest md = MessageDigest.getInstance("MD5");
+
+			// digest() method called
+			// to calculate message digest of an input
+			// and return array of byte
+			byte[] messageDigest = md.digest(input.getBytes());
+
+			// Convert byte array into signum representation
+			BigInteger no = new BigInteger(1, messageDigest);
+
+			// Convert message digest into hex value
+			String hashtext = no.toString(16);
+
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+
+			// Customize by adding dash sign
+			String altered = "";
+			for (int i = 0; i < hashtext.length(); i ++) {
+				altered += hashtext.charAt(i);
+				if (i % 2 != 0) {
+					altered += "-";
+				}
+			}
+			altered = altered.substring(0, altered.length() - 1);
+
+			return altered;
+
+		}
+
+		// For specifying wrong message digest algorithms
+		catch (NoSuchAlgorithmException e) {
+			System.out.println("Exception thrown"
+					+ " for incorrect algorithm: " + e);
+			return null;
+		}
+	}
+
+
+}
+
+/* https://stackoverflow.com/questions/47552835/the-type-webmvcconfigureradapter-is-deprecated */
+/*
+	@Configuration
+	public class WebMvcConfig extends WebMvcConfigurerAdapter {
+	
+		@Bean
+		public BCryptPasswordEncoder passwordEncoder() {
+			BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+			return bCryptPasswordEncoder;
+		}
+	
+	}
+
+*/
