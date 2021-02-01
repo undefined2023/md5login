6b2f3e2afa7e43a99b77acc55adb1369557a2705
heguangbin <undefined2023@outlook.com>
2021-02-01 14:57:38 +0800

    修改建表sql

--- src/main/sql/create_table_file.sql
+++ src/main/sql/create_table_file.sql
@@ -1,30 +1,17 @@
 
+-- create table fileinfo(
+-- 	id int not null auto_increment,
+-- 	name varchar(255) not null,
+-- 	size int not null,
+-- 	upload_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+-- 	uploader_username varchar(255) not null,
+-- 	approver_username varchar(255),
+-- 	approved boolean default false,
+-- 	primary key (id)
+-- )
 
-
-create table fileinfo(
-	id int not null auto_increment,
-	name varchar(255) not null,
-	size int not null,
-	upload_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-	uploader_username varchar(255) not null,
-	approver_username varchar(255),
-	approved boolean default false,
-	primary key (id)
-)
-
-commit
-
-insert into file(name, size, upload_date, uploader_username)
-values('file001', 1000111, '2020-12-17', 'heguangbin')
-
-select * from file
-
-rename table file to fileinfo
-
-
-alter table fileinfo
-change approver_id_username approver_username varchar(255) default null
-
+-- alter table fileinfo
+-- change approver_id_username approver_username varchar(255) default null
 
 -- 以下datetime的定义需要5.6.5以上mysql才支持
 -- 所以date改成timestamp算了
@@ -39,3 +26,13 @@ create table fileinfo(
 	approved boolean default false,
 	primary key (id)
 )
+
+insert into file(name, size, upload_date, uploader_username)
+values('file001', 1000111, '2020-12-17', 'heguangbin')
+
+commit
+
+
+
+
+
